6017c4cc612144d504f30ac2e8f870d8c02055c8
KaibutsuSama <kawaiiashsama@gmail.com>
2023-10-24 19:10:01 -0400

    add: Chapter4

--- Chapter4/arrayone.cpp
+++ Chapter4/arrayone.cpp
@@ -11,5 +11,16 @@ int main() {
     int yamcosts[3] = {20, 30, 5};
     cout << "Total yams = ";
     cout << yams[0] + yams[1] + yams[2] << endl;
+
+    cout << "The package with " << yams[1] << " yams costs ";
+    cout << yamcosts[1] << " cents per yam.\n";
+
+    int total = yams[0] * yamcosts[0] + yams[1] * yamcosts[1];
+    total = total + yams[2] * yamcosts[2];
+    cout << "The total yam expense is " << total << " cents.\n";
+
+    cout << "\nSize of yams array = " << sizeof(yams) << " bytes.";
+    cout << "\nSize of one element = " << sizeof(yams[0]) << " bytes.\n";
+
     return 0;
 }
