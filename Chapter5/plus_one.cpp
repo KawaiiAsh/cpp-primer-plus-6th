e8592ef15d8d0191b920d8a77d57a0bc0a1cc7b6
KaibutsuSama <kawaiiashsama@gmail.com>
2023-11-01 22:24:17 -0400

    add: Chapter5

---
+++ Chapter5/plus_one.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout, std::cin, std::endl;
+
+int main() {
+    int a = 20;
+    int b = 20;
+    cout << "a = " << a << " b = " << b << endl;
+    cout << "a++ = " << a++ << " b++ = " << ++b << endl;
+    cout << "a = " << a << " b = " << b << endl;
+
+    return 0;
+}
