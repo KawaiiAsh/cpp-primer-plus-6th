2e388ea031743d16aa7f718c7b21ff268290c26f
KaibutsuSama <kawaiiashsama@gmail.com>
2023-11-16 20:03:39 -0500

    add: Chapter7

--- Chapter7/atrctfun.cpp
+++ Chapter7/atrctfun.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -12,5 +13,37 @@ struct rect {
     double y;
 };
 
-polar reac_to_polar(rect xypos);
+polar rect_to_polar(rect xypos);
+
 void show_polar(polar dapos);
+
+int main() {
+    rect rplace;
+    polar pplace;
+
+    cout << "Enter the x and y values:";
+    while(cin >> rplace.x >> rplace.y){
+        pplace = rect_to_polar(rplace);
+        show_polar(pplace);
+        cout << "Next two numbers (q to quit):";
+    }
+    cout << "Done.\n";
+    return 0;
+}
+
+polar rect_to_polar(rect xypos) {
+    polar answer;
+
+    answer.distance = sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
+
+    answer.angle = atan2(xypos.y, xypos.x);
+    return answer;
+}
+
+void show_polar(polar dapos){
+    const double Rad_to_deg = 57.29577951;
+
+    cout << "distance = " << dapos.distance;
+    cout << ", angle = " << dapos.angle * Rad_to_deg;
+    cout << " degrees\n";
+}
